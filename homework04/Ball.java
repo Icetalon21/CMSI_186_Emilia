70ba798dd6e8843aa415565f6e8a61ffaa908743
Icetalon21 <39370518+Icetalon21@users.noreply.github.com>
2019-03-05 02:14:53 -0800

    Created Ball class and added constants

---
+++ homework04/Ball.java
@@ -0,0 +1,13 @@
+public class Ball{
+    private double radius;
+    private int weight;
+
+    //public constructor
+    public Ball(){
+        double radius = 4.45;
+        int weight = 1;
+    }
+    public static void main( String[] args ) {
+        System.out.println( "Hello world from the Ball class..." );
+     }
+}
