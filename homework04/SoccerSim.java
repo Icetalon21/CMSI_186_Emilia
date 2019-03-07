9e6284a4e6e8529a17e53dd5e4a4aed4f6b05a24
Icetalon21 <39370518+Icetalon21@users.noreply.github.com>
2019-03-06 23:25:35 -0800

    Added documentation

--- homework04/SoccerSim.java
+++ homework04/SoccerSim.java
@@ -1,21 +1,45 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  SoccerSim.java
+ *  Purpose       :  Provides a class describing the soccer field.
+ *  @author       :  Emilia Huerta
+ *  Date          :  2019-03-05
+ *  Description   :  N/A
+ *
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None Yet
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2019-03-05  Emilia Huerta Created main
+ *  @version 1.0.1  2019-03-06  Emilia Huerta Added documentation
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+
 public class SoccerSim{
     public static void main( String args[] ) {
-        // int highRollCount = 0;
-        // int highRollSides = 0;
-        // int highScore = 0;
-        int startFirstBallX = 0;
-        int startFirstBallY = 0;
-        int firstBallXAxis = 0;
-        int firstBallYAxis = 0;
-        int startSecondBallX = 0;
-        int startSecondBallY = 0;
-        int secondBallXAxis = 0;
-        int secondBallYAxis = 0;
+        double startFirstBallX = 0;
+        double startFirstBallY = 0;
+        double firstBallXAxis = 0;
+        double firstBallYAxis = 0;
+        double startSecondBallX = 0;
+        double startSecondBallY = 0;
+        double secondBallXAxis = 0;
+        double secondBallYAxis = 0;
 
 
         try{
-            highRollCount = Integer.parseInt( args[0] );
-            highRollSides = Integer.parseInt( args[1] );
+            startFirstBallX = Double.parseDouble( args[0] );
+            startFirstBallY = Double.parseDouble( args[1] );
+            firstBallXAxis  = Double.parseDouble( args[2] );
+            firstBallYAxis  = Double.parseDouble( args[3] );
+            startSecondBallX = Double.parseDouble(args[4] );
+            startSecondBallY = Double.parseDouble(args[5] );
+            secondBallXAxis  = Double.parseDouble(args[6] );
+            secondBallYAxis  = Double.parseDouble(args[7] );
         }
         catch(NumberFormatException nfe){System.out.println("Bad input");}
     }
