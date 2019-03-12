e405493ee89c28d3008280bfe7258398a880d7d1
Icetalon21 <39370518+Icetalon21@users.noreply.github.com>
2019-03-12 02:27:44 -0700

    Added an array for the soccerballs
    Also commented out all "second ball" items.  Did this since there should be an arbitrary number of balls, not just two.

--- homework04/SoccerSim.java
+++ homework04/SoccerSim.java
@@ -15,20 +15,23 @@
  *           -----  ----------  ------------  -----------------------------------------------------------
  *  @version 1.0.0  2019-03-05  Emilia Huerta Created main
  *  @version 1.0.1  2019-03-06  Emilia Huerta Added documentation
+ *  @version 1.0.2  2019-03-12  Emilia Huerta Added an array for the soccerballs
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 
 
 public class SoccerSim{
+   private Ball[] soccerBalls = null;
+   private double ballCount = 0;
     public static void main( String args[] ) {
         double startFirstBallX = 0;
         double startFirstBallY = 0;
         double firstBallXAxis = 0;
         double firstBallYAxis = 0;
-        double startSecondBallX = 0;
-        double startSecondBallY = 0;
-        double secondBallXAxis = 0;
-        double secondBallYAxis = 0;
+      //   double startSecondBallX = 0;
+      //   double startSecondBallY = 0;
+      //   double secondBallXAxis = 0;
+      //   double secondBallYAxis = 0;
 
 
         try{
@@ -36,11 +39,27 @@ public class SoccerSim{
             startFirstBallY = Double.parseDouble( args[1] );
             firstBallXAxis  = Double.parseDouble( args[2] );
             firstBallYAxis  = Double.parseDouble( args[3] );
-            startSecondBallX = Double.parseDouble(args[4] );
-            startSecondBallY = Double.parseDouble(args[5] );
-            secondBallXAxis  = Double.parseDouble(args[6] );
-            secondBallYAxis  = Double.parseDouble(args[7] );
+            // startSecondBallX = Double.parseDouble(args[4] );
+            // startSecondBallY = Double.parseDouble(args[5] );
+            // secondBallXAxis  = Double.parseDouble(args[6] );
+            // secondBallYAxis  = Double.parseDouble(args[7] );
         }
         catch(NumberFormatException nfe){System.out.println("Bad input");}
+
+        System.out.println( "\n   Hello world, from the SoccerSim program!!\n\n" ) ;
+        if( 0 == args.length ) {
+           System.out.println( "   Sorry you must enter at least one argument\n");
+           System.exit( 1 );
+        }
+        if( args.length >= 1){
+           double oneArg = Double.parseDouble(args[0]);
+           if(Double.parseDouble(args[0]) > 500){
+              System.out.println("Ball is out of bounds");
+              System.exit(0);
+           }
+           if(2 <= args.length){
+              double twoArg = Double.parseDouble(args[1]);
+           }
+        }
     }
 }
