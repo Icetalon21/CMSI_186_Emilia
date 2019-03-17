a51cc1574939525e6e52bdf7773ffecd99558424
Icetalon21 <39370518+Icetalon21@users.noreply.github.com>
2019-03-17 03:53:20 -0700

    Wrote methods for stillMoving & atRest
    Also added a temporary move method.

--- homework04/Ball.java
+++ homework04/Ball.java
@@ -19,28 +19,35 @@
  *  @version 1.0.3  2019-03-12  Emilia Huerta Wrote method to return current speed of the ball
  *  @version 1.0.4  2019-03-14  Emilia Huerta Tested the location return strings - worked
  *  @version 1.0.5  2019-03-15  Emilia Huerta Tried to incorporate friction for speed after tick - failed
+ *  @version 1.0.6  2019-03-17  Emilia Huerta Wrote methods to see if ball is still moving/at rest
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 //import java.awt.Point;
 // import java.awt.*;
 // import java.awt.geom.Point2D;
 
 public class Ball{
-    private double radius;
-    private double weight;
+    private double radius = 4.45;
+    private double weight = 1.0;
     private double x;
     private double y;
     private double xVel;
     private double yVel;
+    private boolean atRest;
     //private Point2D p = null;
 
     //public constructor
     public Ball(double x, double y, double xVel, double yVel){
-        double radius = 4.45;
-        double weight = 1.0;
+        // double radius = 4.45;
+        // double weight = 1.0;
         this.x = x;
         this.y = y;
         this.xVel = xVel;
         this.yVel = yVel;
+        this.atRest = Math.sqrt(this.xVel * this.xVel + this.yVel * this.yVel) < 0.0833333;
+        //0.0833333 is one inch of a foot
+        //https://msu.edu/~tuckeys1/highschool/physics/p_equations.htm referenced for formula
+        //This equation relates the lengths of the vector and its components.
+        //It is taken directly from the Pythagorean theorem.
     }
     /**
     *  Method to validate the angle argument
@@ -68,6 +75,14 @@ public class Ball{
         return this.y;
     }
 
+    public double getXVelocity(){
+        return this.xVel;
+    }
+
+    public double getYVelocity(){
+        return this.yVel;
+    }
+
     /**
     *  Method to validate the X argument
     @param   argOne String from the main programs args[0] input
@@ -88,9 +103,37 @@ public class Ball{
         return Double.parseDouble(argTwo);
     }
 
-    public String getCurrentSpeed(){
-        return "(" + this.xVel + "," + this.yVel + ")";
+    public boolean atRest(){
+        return this.atRest;
     }
+
+    public boolean stillMoving(){
+        return !this.atRest;
+    }
+
+    public void move(double b){  //got help from TA. Honestly dont understand this movement formula yet.
+        if(!this.atRest){
+            while(b >= 1.0){
+                this.x += this.xVel;
+                this.y += this.yVel;
+                this.xVel -= 0.01 * this.xVel;
+                this.yVel -= 0.01 * this.yVel;
+                b -= 1.0;
+
+            if(b > 0.0){
+                this.x += this.xVel * b;
+                this.y += this.yVel * b;
+                this.xVel -= 0.01 * this.xVel * b;
+                this.yVel -= 0.01 * this.yVel * b;
+            }
+            this.atRest = Math.sqrt(this.xVel * this.xVel + this.yVel * this.yVel) < 0.0833333;
+            }
+        }
+    }
+
+    // public String getCurrentSpeed(){
+    //     return "(" + this.xVel + "," + this.yVel + ")";
+    // }
 
     //  public static double isXInBounds(argOne){
     //     if( -500 < (Integer.parseInt(x) || Integer.parseInt(y) > 500){
@@ -99,7 +142,10 @@ public class Ball{
     //  }
 
     public String locationToString(){
-        return "(" + this.x + "," + this.y + ")";
+        if(this.atRest){
+            return "Ball is located at (" + this.x + "," + this.y + ")";
+        }
+        return "Ball is located at ("+ this.x + "," + this.y + ") at a velocity of (" + this.xVel + "," + this.yVel + ") feet per second";
     }
 
     //https://stackoverflow.com/questions/15990209/game-physics-friction
@@ -138,6 +184,7 @@ public class Ball{
 
     public static void main( String[] args ) {
         System.out.println( "Hello world from the Ball class..." );
+
         // System.out.println(new Point(5,5).toString());
         // Point p = new Point(3,2);
         //     System.out.println(p.toString());
