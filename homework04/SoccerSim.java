830a21142a63ad2d96e797e9196542b8d9f7ac32
Icetalon21 <39370518+Icetalon21@users.noreply.github.com>
2019-03-18 10:16:32 -0700

    Wrote booleans for if a ball was touching/not

--- homework04/SoccerSim.java
+++ homework04/SoccerSim.java
@@ -16,50 +16,128 @@
  *  @version 1.0.0  2019-03-05  Emilia Huerta Created main
  *  @version 1.0.1  2019-03-06  Emilia Huerta Added documentation
  *  @version 1.0.2  2019-03-12  Emilia Huerta Added an array for the soccerballs
+ *  @version 1.0.2  2019-03-17  Emilia Huerta Wrote booleans for if a ball was touching/not
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 
 
 public class SoccerSim{
    private Ball[] soccerBalls = null;
-   private double ballCount = 0;
-    public static void main( String args[] ) {
-        double startFirstBallX = 0;
-        double startFirstBallY = 0;
-        double firstBallXAxis = 0;
-        double firstBallYAxis = 0;
-      //   double startSecondBallX = 0;
-      //   double startSecondBallY = 0;
-      //   double secondBallXAxis = 0;
-      //   double secondBallYAxis = 0;
+   private int ballCount = 0;
+   private double fieldWidth = 500.0;
+   private double fieldHight = 500.0;
+   private boolean stillMoving = true;
+   private Clock clock = new Clock(0);
+   private double timeSlice = 1.0; // for one second
 
+   public static void main( String args[] ) {
+      System.out.println( "\n   Hello world, from the SoccerSim program!!\n\n" );
+      int ballCount = (args.length/4);
+      Ball[] balls = new Ball[ballCount];
+      int j = 0;
+      for(int i = 0; i < args.length; i +=4){
+         try{
+            double ballX = Double.parseDouble( args[i] );
+            double ballY = Double.parseDouble( args[i+1] );
+            double ballXAxis  = Double.parseDouble( args[i+2] );
+            double ballYAxis  = Double.parseDouble( args[i+3] );
+            balls[j] = new Ball(ballX, ballY, ballXAxis, ballYAxis);
+            j++;
+         }
+         catch (Exception exception){
+            throw new IllegalArgumentException("Please enter valid input");
+         }
+      }
+      do {
+         Clock.tick(0.0, 0.0, 0.0, soccerSim.timeSlice);
+         SoccerSim.timeSiceUpdate();
 
-        try{
-            startFirstBallX = Double.parseDouble( args[0] );
-            startFirstBallY = Double.parseDouble( args[1] );
-            firstBallXAxis  = Double.parseDouble( args[2] );
-            firstBallYAxis  = Double.parseDouble( args[3] );
-            // startSecondBallX = Double.parseDouble(args[4] );
-            // startSecondBallY = Double.parseDouble(args[5] );
-            // secondBallXAxis  = Double.parseDouble(args[6] );
-            // secondBallYAxis  = Double.parseDouble(args[7] );
-        }
-        catch(NumberFormatException nfe){System.out.println("Bad input");}
+      } while (true);
+      System.out.println("derp");
+      // Clock clock = new Clock (0, 0, 0.0, SoccerSim.timeSlice); //hours, minutes, & seconds
+      // double BallX = 0;
+      // double BallY = 0;
+      // double BallXAxis = 0;
+      // double BallYAxis = 0;
 
-        System.out.println( "\n   Hello world, from the SoccerSim program!!\n\n" ) ;
-        if( 0 == args.length ) {
-           System.out.println( "   Sorry you must enter at least one argument\n");
-           System.exit( 1 );
-        }
-        if( args.length >= 1){
-           double oneArg = Double.parseDouble(args[0]);
-           if(Double.parseDouble(args[0]) > 500){
-              System.out.println("Ball is out of bounds");
-              System.exit(0);
-           }
-           if(2 <= args.length){
-              double twoArg = Double.parseDouble(args[1]);
-           }
-        }
-    }
+      //   try{
+      //       BallX = Double.parseDouble( args[0] );
+      //       BallY = Double.parseDouble( args[1] );
+      //       BallXAxis  = Double.parseDouble( args[2] );
+      //       BallYAxis  = Double.parseDouble( args[3] );
+
+      //    }
+      //   catch(NumberFormatException nfe){System.out.println("Bad input");}
+
+   }
+         public void handleInitialArguments(String args[]){
+            if( 0 == args.length){
+               System.out.println(" Sorry you must enter in at least four arguments\n");
+               System.exit(0);
+            }
+         }
+
+         static boolean touching(Ball[] ballArray){
+            for(int i = 0; i < ballArray.length -1; ++i){
+               for(int j = i +1; j < ballArray.length; ++j){
+                  double ballX;
+                  double ballY = (ballArray[i].getX() - ballArray[j].getX()) * 12.0;
+                  if(!(ballY * ballY + (ballX = (ballArray[i].getY() - ballArray[j].getY()) * 12) * ballX < 79.21)) continue;
+                  return true;
+               }
+            }
+            return false;
+         }
+
+         static boolean notTouching(Ball[] ballArray){
+            return !SoccerSim.touching(ballArray);
+         }
+
+         public boolean aBallMoving(){
+            for(int i = 0; i < soccerBalls.length; ++i){ //dead code means code that is not used
+               if(!soccerBalls[i].stillMoving());
+                  return true;
+            }
+            return false;
+         }
+
+         public void timeSiceUpdate(){
+            for(int i = 0; i < this.soccerBalls.length; ++i){
+               this.soccerBalls[i].move(this.timeSlice);
+            }
+         }
+
+         // public int[] isTouching(){
+         //    int t;
+         //    int[] check = new int[2];
+         //    double b = 0.0;
+         //    check[0] = 100;
+         //    check[1] = 100;
+         //    for(t = 0; t < this.soccerBalls.length -1; ++t){
+         //       for (int i = t + 1; i < this.soccerBalls.length; ++i){
+         //          b = Math.sqrt(Math.pow(this.soccerBalls[t].getX()[0] - this.soccerBalls[i].getX()[0],2.0) + Math.pow(this.soccerBalls[t].getY()[1] - this.soccerBalls[i].getY()[1], 2.0));
+         //          if (!( b * 12.0 <= 8.9)) continue;
+         //          check[0] = t;
+         //          check[1] = i;
+         //          return check;
+         //       }
+         //    }
+         // }
+
+         // public void validateArgs(String[] args){
+
+         //   }
+
+      //      if( 0 != args.length % 4){
+      //         this.timeSlice = this.clock.validateTimeSliceArg(args[args.length - 1]);
+      //      }
+      //   }
+         //   double oneArg = Double.parseDouble(args[0]);
+         //   if(Double.parseDouble(args[0]) > 500){
+         //      System.out.println("Ball is out of bounds");
+         //      System.exit(0);
+
+         //   if(2 <= args.length){
+         //      double twoArg = Double.parseDouble(args[1]);
+         //   }
 }
