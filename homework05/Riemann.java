5373e075ccedef95a49dcd2831f95318f59d83d5
Icetalon21 <39370518+Icetalon21@users.noreply.github.com>
2019-03-20 14:07:05 -0700

    Created basic constructor

---
+++ homework05/Riemann.java
@@ -0,0 +1,24 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Riemann.java
+ *  Purpose       :  Provides a class describing a single soccer ball.
+ *  @author       :  Emilia Huerta
+ *  Date          :  2019-03-20
+ *  Description   :  This class computes definite integrals using Riemann integration.
+ *
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None Yet
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2019-03-20  Emilia Huerta Created basic constructor
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+ public class Riemann{
+
+    public Riemann( String type, double args, double lowerbound, double upperBound, double upperBoundPercent){
+
+    }
+ }
