64cc1f79d1025c076014e5607a691d9ec52fc375
Emilia Huerta <39370518+Icetalon21@users.noreply.github.com>
2019-03-25 19:55:15 -0700

    Wrote fields; tried to find roots

--- homework05/Riemann.java
+++ homework05/Riemann.java
@@ -15,28 +15,64 @@
  *           -----  ----------  ------------  -----------------------------------------------------------
  *  @version 1.0.0  2019-03-20  Emilia Huerta Created basic constructor
  *  @version 1.0.1  2019-03-25  Emilia Huerta Attempted to deal with "%"
+ *  @version 1.0.2  2019-03-25  Emilia Huerta Wrote fields; tried to find roots
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 public class Riemann{
+   private String type;
+   private double[] coeffs;
+   private double lowerBound;
+   private double upperBound;
+   private double percent;
 
-   public Riemann( String type, int[] coeffs, double lowerbound, double upperBound, double percent){
+   public Riemann( String type, double[] coeffs, double lowerBound, double upperBound, double percent){
+      //handle type
+      //handle coeffs
+   }
+
+   // public static int coefficients (double[] args){
+   //    if()
+   // }
+
+   public void type( String args[]){
+      switch(args[0]){
+         case "poly":
+      }
+   }
+
+   public void getHeight(){
 
    }
 
-   // public static int coefficients (int[] coeffs){
-   //    if
+
+   // static void bisectionMethod(double a, double b){
+   //    double c = a;
+   //    while ((b-a) >= 0.01){
+   //       c = (a + b) / 2;
+   //    }
    // }
 
+   // public static findRoots(double coefficients){
+   //    int N = coefficients.length -1;
+   //    test c = new test (N,N);
+   //    double a = coefficients[N];
+   //    for(int i = 0; i < N; i++){
+   //       c.set(i, N-1, -coefficients[i]/a);
+   //    }
+   //    for(int i = 1; i < N; i++){
+   //       c.set(i,i-1,1);
+   //    }
+   // }
 
    public static void main (String [] args){
       if(args[args.length -1].contains("%")){
-         Double.parseDouble(args[args.length - 1]);
-         Double.parseDouble(args[args.length -1].substring(0, args.length - 1));
+         double percent = Double.parseDouble(args[args.length -1].substring(0, args[args.length -1].length() - 1));
       }
-      
-      switch(args[0]){
-         case "poly": 
+      //make a riemann(blah blah blah, percent)
 
-      }
+      // switch(args[0]){
+      //    case "poly":
+
+      // }
    }
  }
