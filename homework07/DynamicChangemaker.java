7b539a919f1907d7e11a0a0c28fb08bb1767f21f
Emilia Huerta <39370518+Icetalon21@users.noreply.github.com>
2019-04-29 13:45:56 -0700

    Began writing class

---
+++ homework07/DynamicChangemaker.java
@@ -0,0 +1,29 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  DynamicChangemaker.java
+ * Purpose    :  The DynamicChangeMaker class
+ * @author    :  Emilia Huerta
+ * Date       :  2019-04-29
+ * Description:  takes as input arguments a sequence of coin denominations
+ *               [distinct positive integers in no particular order],
+ *               followed by an arbitrary amount of money [a non-negative integer],
+ *               and which outputs the optimal way of making change for that amount
+ *               using those denominations [optimal meaning the minimum number of coins],
+ *               or if change cannot be made, outputs the message IMPOSSIBLE.
+ * Notes      :  None
+ * Warnings   :  None
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Revision History
+ * ================
+ *   Ver      Date     Modified by:  Reason for change or modification
+ *  -----  ----------  ------------  ---------------------------------------------------------------------
+ *  1.0.0  2019-04-29  Emilia Huerta Began writing class
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+public class DynamicChangeMaker{
+  public DynamicChangeMaker(){
+
+  }
+}
